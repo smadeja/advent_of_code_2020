84cd5bfe3fc3c9eb36a6d0e7d27f29b87d52fe67
Szymon Madeja <szymon@mondido.com>
2020-12-05 14:17:32 +0100

    Add a solution for Toboggan Trajectory

---
+++ 03_toboggan_trajectory/toboggan_trajectory.cr
@@ -0,0 +1,88 @@
+class InputParser
+  def initialize(
+    @input : IO::FileDescriptor
+  ); end
+
+  def parsed_input
+    parsed_input = [] of Array(Char)
+
+    @input.each_line do |line|
+      parsed_input << line.chars
+    end
+
+    parsed_input
+  end
+end
+
+class Map
+  private getter data
+
+  def initialize(
+    @data : Array(Array(Char))
+  ); end
+
+  def at(location)
+    data[data_column(location)][data_row(location)]
+  end
+
+  def in_bounds?(location)
+    location.last < row_count
+  end
+
+  private def column_count
+    data.first.size
+  end
+
+  private def row_count
+    data.size
+  end
+
+  private def data_column(location)
+    location.last
+  end
+
+  private def data_row(location)
+    location.first % column_count
+  end
+end
+
+class Toboggan
+  getter location
+
+  private getter map
+  private getter pattern
+
+  def initialize(
+    @map : Map,
+    @pattern : {Int32, Int32},
+    @location : {Int32, Int32} = {0, 0}
+  ); end
+
+  def step
+    @location = {location.first + pattern.first, location.last + pattern.last}
+  end
+
+  def over_tree?
+    map.at(location) == '#'
+  end
+
+  def within_map?
+    map.in_bounds?(location)
+  end
+end
+
+parsed_input = InputParser.new(STDIN).parsed_input
+map = Map.new(parsed_input)
+toboggan = Toboggan.new(map, {3, 1})
+
+tree_count = 0
+
+while toboggan.within_map?
+  if toboggan.over_tree?
+    tree_count += 1
+  end
+
+  toboggan.step
+end
+
+puts tree_count
