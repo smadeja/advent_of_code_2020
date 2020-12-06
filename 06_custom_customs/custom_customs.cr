916b9a0eec5172ca9cf331bb59f3a00f90bf2154
Szymon Madeja <szymon@mondido.com>
2020-12-06 12:03:36 +0100

    Add a solution for Custom Customs

---
+++ 06_custom_customs/custom_customs.cr
@@ -0,0 +1,39 @@
+class InputParser
+  def initialize(
+    @input : IO::FileDescriptor
+  ); end
+
+  def parsed_input
+    parsed_input = [] of Array(Set(Char))
+
+    while chunk = @input.gets("\n\n")
+      parsed_input << chunk.split.map do |answer_string|
+        Set.new(answer_string.each_char)
+      end
+    end
+
+    parsed_input
+  end
+end
+
+class CustomsDeclaration
+  def initialize(
+    @data : Array(Set(Char))
+  ); end
+
+  def count
+    answers.size
+  end
+
+  private def answers
+    @data.reduce { |acc, answer_set| acc + answer_set }
+  end
+end
+
+parsed_input = InputParser.new(STDIN).parsed_input
+
+sum = parsed_input.sum do |custom_data|
+  CustomsDeclaration.new(custom_data).count
+end
+
+puts sum
